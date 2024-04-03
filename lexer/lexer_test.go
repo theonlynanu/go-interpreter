997b87d441136934000ae3ecaa407b9bd9f0b5c7
Danyal <112341859+theonlynanu@users.noreply.github.com>
2024-04-03 15:35:29 -0400

    Write second test for lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,23 +6,91 @@ import (
 	"github.com/theonlynanu/go-interpreter/token"
 )
 
-func TestNextToken(t *testing.T) {
-	input := "=+(){},;"
+type tokenTestType struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
 
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
+func TestNextToken(t *testing.T) {
+
+	t.Run("Basic syntax recognition", func(t *testing.T) {
+		input := "=+(){},;"
+
+		tests := []tokenTestType{
+			{token.ASSIGN, "="},
+			{token.PLUS, "+"},
+			{token.LPAREN, "("},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.RBRACE, "}"},
+			{token.COMMA, ","},
+			{token.SEMICOLON, ";"},
+			{token.EOF, ""},
+		}
+
+		assertTokens(t, input, tests)
+	})
+
+	t.Run("Initialize variables and functions", func(t *testing.T) {
+		input := `let five = 5;
+		let ten = 10;
+		
+		let add = function (x, y) {
+			x + y;
+		};
+		
+		let result = add(five, ten)`
+
+		tests := []tokenTestType{
+			{token.LET, "let"},
+			{token.IDENT, "five"},
+			{token.ASSIGN, "="},
+			{token.INT, "5"},
+			{token.SEMICOLON, ";"},
+
+			{token.LET, "let"},
+			{token.IDENT, "ten"},
+			{token.ASSIGN, "="},
+			{token.INT, "10"},
+			{token.SEMICOLON, ";"},
+
+			{token.LET, "let"},
+			{token.IDENT, "add"},
+			{token.ASSIGN, "="},
+			{token.FUNCTION, "function"},
+			{token.LPAREN, "("},
+			{token.IDENT, "x"},
+			{token.COMMA, ","},
+			{token.IDENT, "y"},
+			{token.RPAREN, ")"},
+
+			{token.LBRACE, "{"},
+			{token.IDENT, "x"},
+			{token.PLUS, "+"},
+			{token.IDENT, "y"},
+			{token.SEMICOLON, ";"},
+			{token.RBRACE, "}"},
+			{token.SEMICOLON, ";"},
+
+			{token.LET, "let"},
+			{token.IDENT, "result"},
+			{token.ASSIGN, "="},
+			{token.IDENT, "add"},
+			{token.LPAREN, "("},
+			{token.IDENT, "five"},
+			{token.COMMA, ","},
+			{token.IDENT, "ten"},
+			{token.RPAREN, ")"},
+			{token.SEMICOLON, ";"},
+			{token.EOF, ""},
+		}
+
+		assertTokens(t, input, tests)
+	})
+}
+
+func assertTokens(t testing.TB, input string, tests []tokenTestType) {
+	t.Helper()
 
 	l := New(input)
 	for i, tt := range tests {
