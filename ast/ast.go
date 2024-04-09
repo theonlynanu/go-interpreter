29796682345552899ca3be59543a2fcaf98e52dc
Danyal <112341859+theonlynanu@users.noreply.github.com>
2024-04-09 19:37:03 -0400

    Add let statement parsing + parser error handling

--- ast/ast.go
+++ ast/ast.go
@@ -19,11 +19,11 @@ type Expression interface {
 	expressionNode()
 }
 
+// Root program node for any given AST
 type Program struct {
 	Statements []Statement
 }
 
-// Root program node for any given AST
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -55,4 +55,15 @@ func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+type ReturnStatement struct {
+	Token       token.Token // the 'RETURN' token
+	ReturnValue Expression
+}
+
+// Satisfies the statement interface
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/theonlynanu/go-interpreter/ast"
 	"github.com/theonlynanu/go-interpreter/lexer"
 	"github.com/theonlynanu/go-interpreter/token"
@@ -9,12 +11,17 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors []string
+
 	currToken token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Reads two tokens so currToken and peekToken are set to start
 	p.nextToken()
@@ -28,7 +35,82 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
-// Placeholder for writing test
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	// expectPeek advances the tokens...
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// ...and so, we can use currToken here
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: Right now we're skipping expressions until parser encounters a semicolon
+
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool {
+	return p.currToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// Checks peekToken's type and progresses tokens
+//
+// Advancing the token like this prevents advancement without asserting
+// a valid peekToken type
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+// Parses tokens in program and returns the root node
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("Expected next token to be %s, got %s instead.", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
